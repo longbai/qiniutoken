730192c99bf3fe9a8fd2f631d3dd829aeded7abb
longbai <slongbai@gmail.com>
2014-06-15 07:12:47 +0800

    add uptoken/downtoken

--- src/qiniutoken/main.go
+++ src/qiniutoken/main.go
@@ -1,40 +1,79 @@
 package main
 
 import (
-	// "encoding/base64"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
-	// "net/url"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 import (
+	"github.com/qiniu/api/auth/digest"
 	"github.com/qiniu/api/rs"
 )
 
-func decodeUpToken(token string) (data rs.PutPolicy, err error) {
+func decodeUpToken(token string) (policy rs.PutPolicy, err error) {
+	array := strings.Split(token, ":")
+	if len(array) != 3 {
+		err = errors.New("invalid token")
+		return
+	}
+
+	data, err := base64.URLEncoding.DecodeString(array[2])
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(data, &policy)
 	return
 }
 
-func encodeUpToken(jsonData string, ak, sk string) (token string, err error) {
+func encodeUpToken(args []string, ak, sk string) (token string, err error) {
 	var policy rs.PutPolicy
-	err = json.Unmarshal([]byte(jsonData), &policy)
+	for _, v := range args {
+		arg := strings.Split(v, "=")
+		if len(arg) != 2 {
+			err = errors.New("invalid args " + v)
+			return
+		}
+		switch arg[0] {
+		case "Scope":
+			policy.Scope = arg[1]
+		case "Expires":
+			data, err1 := strconv.ParseUint(arg[1], 10, 32)
+			if err1 != nil {
+				err = err1
+				return
+			}
+			policy.Expires = uint32(data)
+		}
+	}
+	mac := digest.Mac{ak, []byte(sk)}
+	token = policy.Token(&mac)
 	return
 }
 
-func encodeDownToken(baseUrl string, ak, sk string, seconds uint64) (token string, err error) {
+func encodeDownToken(baseUrl string, ak, sk string, seconds uint64) (downUrl string, err error) {
+	var policy rs.GetPolicy
+	policy.Expires = uint32(seconds)
+	mac := digest.Mac{ak, []byte(sk)}
+	downUrl = policy.MakeRequest(baseUrl, &mac)
 	return
 }
 
 func main() {
 	upToken := flag.String("uptoken", "", "upload token")
-	upPolicy := flag.String("uppolicy", "", "upload policy json string")
+	upPolicy := flag.Bool("uppolicy", false, "upload policy")
 	accessKey := flag.String("ak", "", "access key")
 	secretKey := flag.String("sk", "", "secret key")
 
 	downUrl := flag.String("downurl", "", "download url for sign")
 	expiredDuration := flag.Uint64("expires", 0, "expired duration seconds")
+	flag.Parse()
 
 	if *upToken != "" {
 		upDecode, err := decodeUpToken(*upToken)
@@ -44,16 +83,18 @@ func main() {
 			os.Exit(1)
 		}
 		out, _ := json.MarshalIndent(upDecode, "", "")
-		fmt.Println(out)
+		fmt.Println(string(out))
+		fmt.Println(time.Unix(int64(upDecode.Expires), 0))
 		return
 	}
 
-	if *upPolicy != "" {
+	if *upPolicy {
 		if *accessKey == "" || *secretKey == "" {
+			fmt.Println("no ak or sk")
 			flag.PrintDefaults()
 			os.Exit(1)
 		}
-		token, err := encodeUpToken(*upPolicy, *accessKey, *secretKey)
+		token, err := encodeUpToken(flag.Args(), *accessKey, *secretKey)
 		if err != nil {
 			fmt.Println(err)
 			flag.PrintDefaults()
@@ -65,6 +106,7 @@ func main() {
 
 	if *downUrl != "" {
 		if *accessKey == "" || *secretKey == "" {
+			fmt.Println("no ak or sk")
 			flag.PrintDefaults()
 			os.Exit(1)
 		}
@@ -81,5 +123,6 @@ func main() {
 		fmt.Println(token)
 		return
 	}
+	flag.PrintDefaults()
 	return
 }
